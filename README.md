0b5f4a9e3386f194d0e969fbe652ed8225cb5507
Unknown <Martin.Beaussart@etu.unige.ch>
2018-04-01 16:34:32 +0200

    test

--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 ```
 # please clean your docker-related stuff (containers, images, network):
 https://gist.github.com/bastman/5b57ddb3c11942094f8d0a97d461b430
+docker ps
+docker kill "name find before"
+
+
 
 sudo mkdir /opt/docker-deploy
 sudo chmod -R 755 /opt/docker-deploy
--- unihub/users-service/src/main/java/ch/unihub/business/service/UserServiceImpl.java
+++ unihub/users-service/src/main/java/ch/unihub/business/service/UserServiceImpl.java
@@ -8,7 +8,9 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.validation.constraints.NotNull;
 
 import ch.unihub.dom.User;
 
@@ -31,7 +33,52 @@ public class UserServiceImpl implements UserService {
 		TypedQuery<User> query = entityManager.createQuery(c);
 		return query.getResultList();
 	}
-	
+
+	@Override
+	public User getUser(String username) {
+		CriteriaBuilder qb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<User> cq = qb.createQuery(User.class);
+
+		Root<User> root = cq.from(User.class);
+		Predicate usernameCond = qb.equal(root.get("username"), username);
+		cq.where(usernameCond);
+		TypedQuery<User> query = entityManager.createQuery(cq);
+		return query.getSingleResult();
+	}
+
+	@Override
+	public User getUser(Long id) {
+		CriteriaBuilder qb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<User> cq = qb.createQuery(User.class);
+
+		Root<User> root = cq.from(User.class);
+		Predicate idCond = qb.equal(root.get("id"), id);
+		cq.where(idCond);
+		TypedQuery<User> query = entityManager.createQuery(cq);
+		return query.getSingleResult();
+	}
+
+	@Override
+	public void addUser(@NotNull User user) {
+		user.setId(null);
+		entityManager.persist(user);
+	}
+
+	@Override
+	public void deleteUser(Long id) {
+		// TODO
+	}
+
+	@Override
+	public void deleteUser(String username) {
+		// TODO
+	}
+
+	@Override
+	public void updateUser(User updatedUser) {
+		// TODO
+	}
+
 	@Override
 	public int getNbUsers() {
 		return getAll().size();
