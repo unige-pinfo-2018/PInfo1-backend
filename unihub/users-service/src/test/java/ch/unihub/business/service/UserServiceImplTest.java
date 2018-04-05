7e05daae47e585721697fc69ecd4691a994cb0a9
Martin Jérémie <martin.jeremie42@gmail.com>
2018-04-05 16:48:52 +0200

    Configured the project to be able to make JUnits 4 tests, and added few empty tests

---
+++ unihub/users-service/src/test/java/ch/unihub/business/service/UserServiceImplTest.java
@@ -0,0 +1,41 @@
+package ch.unihub.business.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserServiceImplTest {
+
+	@Test
+	public void testGetAll() {
+	}
+
+	@Test
+	public void testGetUserString() {
+	}
+
+	@Test
+	public void testGetUserLong() {
+	}
+
+	@Test
+	public void testAddUser() {
+	}
+
+	@Test
+	public void testDeleteUserLong() {
+	}
+
+	@Test
+	public void testDeleteUserString() {
+	}
+
+	@Test
+	public void testUpdateUser() {
+	}
+
+	@Test
+	public void testGetNbUsers() {
+	}
+
+}
---
+++ unihub/users-service/src/test/java/ch/unihub/dom/UserTest.java
@@ -0,0 +1,54 @@
+package ch.unihub.dom;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void testCopyFields() {
+		
+	}
+
+	@Test
+	public void testGetId() {
+		
+	}
+
+	@Test
+	public void testSetId() {
+		
+	}
+
+	@Test
+	public void testGetUsername() {
+		
+	}
+
+	@Test
+	public void testSetUsername() {
+		
+	}
+
+	@Test
+	public void testGetPassword() {
+		
+	}
+
+	@Test
+	public void testSetPassword() {
+		
+	}
+
+	@Test
+	public void testGetEmail() {
+		
+	}
+
+	@Test
+	public void testSetEmail() {
+		
+	}
+
+}
