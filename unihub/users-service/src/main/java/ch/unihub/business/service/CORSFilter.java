ac12a71f2e76f1b67dc0a4d018b3d3c543f678bd
Arthur Deschamps <arthur.deschamps1208@hotmail.com>
2018-05-09 16:41:58 +0200

    Modify CORS filter to allow both localhost and 127.0.0.1. It's a start, even though it's just a drop in the ocean..

--- unihub/users-service/src/main/java/ch/unihub/business/service/CORSFilter.java
+++ unihub/users-service/src/main/java/ch/unihub/business/service/CORSFilter.java
@@ -8,6 +8,7 @@ package ch.unihub.business.service;
   on calling a rest service from domain1 to domain2 will fail
 */
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.container.ContainerRequestContext;
@@ -15,14 +16,31 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
+
+    private static final List<String> allowedOrigins = new ArrayList<>(Arrays.asList(
+            "http://127.0.0.1:8080",
+            "127.0.0.1:8080",
+            "http://localhost:8080",
+            "localhost:8080"
+    ));
+    private static final String defaultOrigin = "http://127.0.0.1:8080";
+
+    private static final Logger logger = LoggerFactory.getLogger(CORSFilter.class);
+
     @Override
     public void filter(final ContainerRequestContext requestContext,
                        final ContainerResponseContext cres) throws IOException {
-        LoggerFactory.getLogger(CORSFilter.class).info("CORS filter triggered: " + requestContext.getUriInfo().getPath());
-        cres.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:8080");
+        final String requestOrigin = requestContext.getHeaders().getFirst("Origin");
+        logger.info("CORS filter triggered; origins = " + requestOrigin + ", request = "
+                + requestContext.getUriInfo().getPath());
+        final String acceptedOrigin = allowedOrigins.contains(requestOrigin) ? requestOrigin : defaultOrigin;
+        cres.getHeaders().add("Access-Control-Allow-Origin", acceptedOrigin);
         cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
         cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
